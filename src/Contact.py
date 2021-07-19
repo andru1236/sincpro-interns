78c8121b7f38e7fb2e7a9fc3c60cc2c5e37fbb06
Jeffer <79720522+JefferBarrera@users.noreply.github.com>
2021-07-19 15:49:27 -0500

    Update Contact.py

--- src/Contact.py
+++ src/Contact.py
@@ -4,12 +4,12 @@ class Contact:
 
     countContactId = 0
 
-    def __init__(self, name, nickname, contactInfo, preferred):
+    def __init__(self, name, nickname, phone, email, address, preferred):
         Contact.countContactId += 1
         self.__contactId = Contact.countContactId
         self.__name = name
         self.__nickname = nickname
-        self.__contactInfo = contactInfo
+        self.__contactInfo = list((phone, email, address))
         self.__preferred = preferred
 
     @property
@@ -19,31 +19,41 @@ class Contact:
     @property
     def name(self):
         return self.__name
-    
+
     @name.setter
     def name(self, name):
         self.__name = name
-    
+
     @property
     def nickname(self):
         return self.__nickname
-    
+
     @nickname.setter
     def nickname(self, nickname):
         self.__nickname = nickname
-    
+
     @property
-    def contactoInfo(self):
+    def contactInfo(self):
         return self.__contactInfo
-    
+
     @contactInfo.setter
     def contactInfo(self, contactInfo):
         self.__contactInfo = contactInfo
-    
+
     @property
     def preferred(self):
-        return self.__preferred
-    
+        return self.preferred
+
     @preferred.setter
-    def prefered(self, preferred):
-        self.__preferred = preferred
+    def preferred(self, preferred):
+        self.preferred = preferred
+
+    def __str__(self):
+        return f"""
+        Contacto: idContacto={self.contactId},
+        nombre={self.name},
+        apodo={self.nickname},
+        telefono={self.contactInfo[0]},
+        email={self.contactInfo[1]},
+        direccion={self.contactInfo[2]},
+        preferido={self.preferred}"""

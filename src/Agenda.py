62613c2146460e8bab6f4b7f8155333b6585da63
Jeffer Barrera <jeffer.barrera.c@gmail.com>
2021-07-20 14:17:15 -0500

    correciones

--- src/Agenda.py
+++ src/Agenda.py
@@ -23,8 +23,8 @@ class Agenda:
 
 if __name__ == "__main__":
 	contacto1 = Contact("Juan Diaz", "Juan", 57, 123456, "juan@mail.com", "cra78", True)
-	contacto2 = Contact("Juan Diaz", "Juan", 57, 123456, "juan@mail.com", "cra78", True)
-	contacto3 = Contact("Juan Diaz", "Juan", 57, 123456, "juan@mail.com", "cra78", True)
+	contacto2 = Contact("Carlos Perez", "Charles", 591, 123456, "carlos@mail.com", "cra78", True)
+	contacto3 = Contact("Leidy Martinez", "Lady", 52, 123456, "leidy@mail.com", "cra78", False)
 	agenda1 = Agenda()
 	agenda1.agregarContacto(contacto1)
 	agenda1.agregarContacto(contacto2)

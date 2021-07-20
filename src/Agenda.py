733fbd3ce2994c3976d88b4a237cd2d485f8633e
Jeffer Barrera <jeffer.barrera.c@gmail.com>
2021-07-20 13:49:20 -0500

    correciones agenda

--- src/Agenda.py
+++ src/Agenda.py
@@ -1,12 +1,13 @@
+
 class Agenda:
 	contactos = []
 
-	def addContacto(a):
-		contactos.append(a)
+	def addContacto(self, a):
+		self.contactos.append(a)
 
-	def buscarContacto(a):
+	def buscarContacto(self, a):
 		for contacto in contactos:
 			if contacto.getPreferredInfo() == a and contactos.len() > 0:
 				return contacto
 			else:
-				print "No es posible buscar el contacto solicitado"
+				print ("No es posible buscar el contacto solicitado")

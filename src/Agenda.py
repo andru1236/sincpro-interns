ea87ac33000e2276ecc809c4f802aba04a98db8f
Jeffer Barrera <jeffer.barrera.c@gmail.com>
2021-07-21 14:35:42 -0500

    merge rama jairo

--- src/Agenda.py
+++ src/Agenda.py
@@ -1,24 +1,24 @@
 
 class Agenda:
 
-	contactos = []
+    contactos = []
 
-	def addContact(self, a):
-		self.contactos.append(a)
-	
-	def DeleteContact(self, id):
-			DeleteContact = [contact for contact in self.contactos if contact['id'] == int(id)]
-			if len(DeleteContact) > 0:
-				self.contactos.remove(DeleteContact[0])
-				return DeleteContact
-			else:
-				return {"messaje": "Contact not found"}
+    def addContact(self, a):
+        self.contactos.append(a)
 
-	def findContact(self, from Contact import Contacta):
-			found = [item for item in self.contactos if item['id'] == int(a)]
-			if(len(found) > 0):
-				return found
-			else: 
-				print(type(a))
-				return {"mensaje": "not found"}
-
+    def DeleteContact(self, id):
+        DeleteContact = [
+            contact for contact in self.contactos if contact['id'] == int(id)]
+        if len(DeleteContact) > 0:
+            self.contactos.remove(DeleteContact[0])
+            return DeleteContact
+        else:
+            return {"messaje": "Contact not found"}
+			
+	def findContact(self, from Contact import Contact):
+        found = [item for item in self.contactos if item['id'] == int(a)]
+        if(len(found) > 0):
+            return found
+        else:
+            print(type(a))
+            return {"mensaje": "not found"}

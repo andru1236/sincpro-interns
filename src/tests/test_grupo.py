6c3c696b3ad5df0106d66f400f779558c2f06468
Jeffer Barrera <jeffer.barrera.c@gmail.com>
2021-07-23 17:27:11 -0500

    unit test contacto corregida

--- src/tests/test_grupo.py
+++ src/tests/test_grupo.py
@@ -2,21 +2,20 @@ import pytest
 from Grupo import Grupo
 from Contact import Contact
 
-# def test_add_contact_group():
-#     FAMILIA = Grupo("Familia")
-#     AMIGOS = Grupo("Amigos")
-#     contacto1 = Contact("Juan Perez", "juanito", 57, 123456, "juan@mail.com", "cra100", "email")
-#     contacto2= Contact("Leidy Martinez", "lady", 52, 789456, "lady@mail.com", "cra50", "telefono")
-#     FAMILIA.add_contact_group(contacto1)
-#     AMIGOS.add_contact_group(contacto1)
-#     FAMILIA.add_contact_group(contacto2)
-#     AMIGOS.add_contact_group(contacto2)
+def test_add_contact_group():
+    FAMILIA = Grupo("Familia")
+    AMIGOS = Grupo("Amigos")
+    contacto1 = Contact("Juan Perez", "juanito", 57, 123456, "juan@mail.com", "cra100", "email")
+    contacto2= Contact("Leidy Martinez", "lady", 52, 789456, "lady@mail.com", "cra50", "telefono")
+    FAMILIA.add_contact_group(contacto1)
+    AMIGOS.add_contact_group(contacto1)
+    FAMILIA.add_contact_group(contacto2)
+    AMIGOS.add_contact_group(contacto2)
 
-#     assert isinstance(FAMILIA.lista[0], Contact) == True
-#     assert isinstance(FAMILIA.lista[1], Contact) == True
-#     assert isinstance(AMIGOS.lista[0], Contact) == True
-#     assert isinstance(AMIGOS.lista[1], Contact) == True
-
+    assert isinstance(FAMILIA.lista[0], Contact) == True
+    assert isinstance(FAMILIA.lista[1], Contact) == True
+    assert isinstance(AMIGOS.lista[0], Contact) == True
+    assert isinstance(AMIGOS.lista[1], Contact) == True
 
 
 def test_remove_contact_group():
@@ -27,4 +26,4 @@ def test_remove_contact_group():
     TRABAJO.add_contact_group(contacto4)
     TRABAJO.remove_contact_group(1)
 
-    assert len(TRABAJO.lista) == 1
+    assert len(TRABAJO.lista) == 1 

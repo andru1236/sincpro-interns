372f14a46110088894348160f414085eeb13a540
Jeffer Barrera <jeffer.barrera.c@gmail.com>
2021-07-23 19:38:37 -0500

    agregando unit test clase grupo

--- src/tests/test_grupo.py
+++ src/tests/test_grupo.py
@@ -2,11 +2,14 @@ import pytest
 from Grupo import Grupo
 from Contact import Contact
 
+
 def test_add_contact_group():
     FAMILIA = Grupo("Familia")
     AMIGOS = Grupo("Amigos")
-    contacto1 = Contact("Juan Perez", "juanito", 57, 123456, "juan@mail.com", "cra100", "email")
-    contacto2= Contact("Leidy Martinez", "lady", 52, 789456, "lady@mail.com", "cra50", "telefono")
+    contacto1 = Contact("Juan Perez", "juanito", 57, 123456,
+                        "juan@mail.com", "cra100", "email")
+    contacto2 = Contact("Leidy Martinez", "lady", 52, 789456,
+                        "lady@mail.com", "cra50", "telefono")
     FAMILIA.add_contact_group(contacto1)
     AMIGOS.add_contact_group(contacto1)
     FAMILIA.add_contact_group(contacto2)
@@ -19,11 +22,18 @@ def test_add_contact_group():
 
 
 def test_remove_contact_group():
-    TRABAJO = Grupo("Trabajo")
-    contacto3 = Contact("Juan Perez", "juanito", 57, 123456, "juan@mail.com", "cra100", "email")
-    contacto4 = Contact("Leidy Martinez", "lady", 52, 789456, "lady@mail.com", "cra50", "telefono")
-    TRABAJO.add_contact_group(contacto3)
-    TRABAJO.add_contact_group(contacto4)
-    TRABAJO.remove_contact_group(1)
+    FAMILIA = Grupo("Familia")
+    FAMILIA.lista = [Contact("Juan Perez", "juanito", 57, 123456, "juan@mail.com", "cra100", "email"),
+                     Contact("Leidy Martinez", "lady", 52, 789456, "lady@mail.com", "cra50", "telefono")]
+    tam_original = len(FAMILIA.lista)
+    delete = FAMILIA.lista[0].contactId
+    FAMILIA.remove_contact_group(delete)
+    assert len(FAMILIA.lista) == tam_original - 1
 
-    assert len(TRABAJO.lista) == 1 
+
+def test_conver_group():
+    FAMILIA = Grupo("Familia")
+    FAMILIA.lista = [Contact("Juan Perez", "juanito", 57, 123456, "juan@mail.com", "cra100", "email"),
+                     Contact("Leidy Martinez", "lady", 52, 789456, "lady@mail.com", "cra50", "telefono")]
+    assert isinstance(FAMILIA.conver_group()["list"][0], Contact)
+    assert isinstance(FAMILIA.conver_group()["list"][1], Contact)
